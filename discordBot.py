0d2ee145fccc251da1c49c9917f505538eaeaaab
Kreachers <dms.gamer@gmail.com>
2020-12-04 20:09:45 -0700

    move bot tooken to separate file and added it to .gitignore. switch away from class format to client.event format

--- discordBot.py
+++ discordBot.py
@@ -1,16 +1,38 @@
 import discord
+import json
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as', self.user)
+info = json.load(open('./info.json', 'r'))
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
+client = discord.Client()
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+@client.event
+async def on_ready():
+    print('Logged on as', client.user)
 
-client = MyClient()
-client.run('Nzg0MjU4ODExNjc3OTAwODYw.X8mr7w.0NzRVDU2U8bkmxVAJIv5Qmv1zeU')
+@client.event
+async def on_message(message):
+    print('Author: ', message.author)
+    print('Content: ', message.content, '\n')
+    # print('message: \n', message)
+    # don't respond to ourselves
+    if message.author == client.user:
+        return
+
+    if message.content == 'ping':
+        await message.channel.send('pong')
+    if message.content == 'right':
+        await message.delete()
+        await message.channel.send('https://media.giphy.com/media/jeXiz1RAvzX44/giphy.gif')
+    # if message.channel.nsfw == True:
+        # await message.channel.send('https://media.giphy.com/media/UVXGjSQNI88XS/giphy.gif')
+
+client.run(info["token"])
+
+#from discord.ext import commands
+#
+#bot = commands.Bot(command_prefix='>')
+#
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send('pong')
+#
